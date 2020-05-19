f621213be63dc90c76a15a6016fbcd2a6216f452
Kevin Fang <reddacc@gmail.com>
2020-05-18 19:15:43 -0500

    update guesser based off porportional length

--- ipaimperial/convert.py
+++ ipaimperial/convert.py
@@ -36,19 +36,20 @@ def convert(sentence):
     print(sentence)
     i = 0
     final_sentence = ""
-    translate_dict = {**sylla[0], **sylla[1]}
+    # translate_dict = {**sylla[0], **sylla[1]}
+    translate_dict = sylla[0]
     while i < len(sentence):
-        bestop, jj = None, None
+        bestop, jj, lenj = None, None, 0
         found = False
-        # for j in range(i + 5, i + 1, -1):
-        for j in range(i + 2, i + 5):
+        for j in range(i + 7, i + 1, -1):
+        # for j in range(i + 2, i + 5):
             word = sentence[i:j]
             for key in translate_dict.keys():
-                if (bestop is not None and len(bestop) < len(translate_dict[key])):
+                if (bestop is not None and lenj < len(translate_dict[key]) - (j - i)):
                     continue
                 if (key.startswith(word)):
-                    if (bestop is None or len(translate_dict[key]) < len(bestop)):
-                        bestop, jj = translate_dict[key], j
+                    if (bestop is None or len(translate_dict[key]) - (j - i) < lenj):
+                        bestop, jj,lenj = translate_dict[key], j, len(key) + j-i
                     found = True
             if (found):
                 break
@@ -64,6 +65,9 @@ def convert(sentence):
 
 if __name__ == "__main__":
     vals = 'neka malɯl not̚nɯn kʌt̚to . nʌɰi tɕak̚ɯn maltʰuto . nap͈ɯtɕi anɯnkʌl'
+    # vals = 'malʌpi sonɯl tɕap̚ko . tɕokɯmɯn nolɾɛto . siltɕika anɯnkʌl'
     # vals = 'mulkamtɕʰʌɾʌm pʰaɾat̚tʌn . hanɯlɯn pʌls͈ʌ k͈amat̚ko . kamtɕʌŋɯn tʌ kiᇁʌtɕjʌ'
+    # vals = 'uɾi tulman nɯk͈jʌtɕinɯn isaŋhan nɯk͈im . nato nʌmu tɕot̚a'
+    # vals = 'asywʌ pʌls͈ʌ si ʌt͈ʌk̚hɛ pʌls͈ʌ sine ponɛtɕuki silɯnkʌl alko it̚ʌ '
     print(vals)
     print(convert(vals))
