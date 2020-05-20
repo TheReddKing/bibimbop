baef82d4611d0fe04e5be6271fe4bf0e1396600e
Kevin Fang <reddacc@gmail.com>
2020-05-20 17:37:16 -0500

    please i have no idea what i'm doing

--- ipaimperial/initialize.sh
+++ ipaimperial/initialize.sh
@@ -1,3 +1,5 @@
 mkdir data
 wget -O data/dictionary.txt https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt
+wget -O data/dictionary_small.txt https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt
+wget -O data/dictionary_2.txt https://gist.githubusercontent.com/h3xx/1976236/raw/bbabb412261386673eff521dddbe1dc815373b1d/wiki-100k.txt
 git clone https://github.com/mphilli/English-to-IPA englishipa
